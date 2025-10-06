96a816895f54f0b386326457f4781a16d89c2424
Gabriel Lima <gabriellimatrabalho1@gmail.com>
2025-10-06 09:40:14 -0300

    Add initial HTML and CSS files for various projects
    - Created index.html and styles.css for the calculadora project.
    - Added experience.html and associated images for the exer_html project.
    - Implemented index.html for the exer_portifolio project.
    - Developed form.html for the form_html project.
    - Established style.css for the guia_turistico project and created guia.html.
    - Removed outdated index2.html from guia_turistico project.
    - Added multiple images for the guia_turistico project.
    - Introduced script.js for the tigrinho project to manage money logic.

---
+++ tigrinho/script.js
@@ -0,0 +1,12 @@
+let dinheiro = 100;
+
+function tigrinho() {
+  for (let i = dinheiro; i > 0; i--) {
+    console.log(`Dinheiro restante: ${i}`);
+  }
+  console.log('Dinheiro acabou');
+}
+
+tigrinho();
+
+
